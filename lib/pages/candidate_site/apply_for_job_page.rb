3eac81b071110fdd08ad0edccd316e4544912c60
SANJEEV GUPTA <sanjeev.gupta@findly.com>
2015-05-04 10:33:24 +0530

    making chnages related to dates join
    committ

--- lib/pages/candidate_site/apply_for_job_page.rb
+++ lib/pages/candidate_site/apply_for_job_page.rb
@@ -181,7 +181,7 @@ def fill_apply_for_job_form
 	select(	"25", :from=>"work_history.start_date_id.1")
     select(	"2010", :from=>"work_history.start_date_id.2")
 
-	select(	"03", :from=>"work_history.end_date_id.0")
+	select(	"04", :from=>"work_history.end_date_id.0")
 	select(	"25", :from=>"work_history.end_date_id.1")
     select(	"2015", :from=>"work_history.end_date_id.2")
 	work_history_company_name.set options[:company]
